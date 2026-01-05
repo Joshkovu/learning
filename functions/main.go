833bcda77d516fe8cf17b0b7cd2cbc88d8393771
Joshkovu <joashkuteesa223@gmail.com>
2026-01-05 08:43:08 +0300

    feat:Applied functions and variables

---
+++ functions/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func printName() {
+	var name string
+	fmt.Println("Enter your name:")
+	fmt.Scan(&name)
+	fmt.Println("Hello ", name)
+}
+
+func printAge() {
+	var age int
+	fmt.Println("Enter your age ")
+	fmt.Scan(&age)
+	fmt.Println("You are ", age, " years old ")
+}
+
+func printCountry() {
+	var country string
+	fmt.Println("Enter your country:")
+	fmt.Scan(&country)
+	fmt.Println("You are from ", country)
+}
+func add(a, b int) int {
+
+	return a + b
+}
+func greet() {
+	names := []string{"Joash", "Mary"}
+	fmt.Scan(names)
+	fmt.Println("Hello", os.Args[1])
+}
+func main() {
+	var k int
+	var m int
+
+	fmt.Println("Enter the value of k:")
+	fmt.Scan(&k)
+	fmt.Println("Enter the value of m:")
+	fmt.Scan(&m)
+	printName()
+	printAge()
+	printCountry()
+	add(k, m)
+	greet()
+}
