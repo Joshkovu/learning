9dede916982134a39ae01e7a2b095fdb7cda79f7
Joshkovu <joashkuteesa223@gmail.com>
2026-01-05 10:00:48 +0300

    feat:implemented looping

--- Loops/main.go
+++ Loops/main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func printEvenNumbers() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for number := range numbers {
+	for number, err := range numbers {
 		if number%2 == 0 {
 			fmt.Println(number)
+		}
+		if err < 0 {
+			fmt.Println("Error", err)
 		}
 	}
 }
