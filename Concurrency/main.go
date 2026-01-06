73bf79920c4732de753e91148866213c16a10ae4
Joshkovu <joashkuteesa223@gmail.com>
2026-01-06 12:06:59 +0300

    feat:implement http requests , json and crud api

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
-func printNumbers() {
-	numbers := []int{1, 2, 3, 4, 5}
-	for _, number := range numbers {
-		fmt.Println(number)
-		time.Sleep(1 * time.Second)
-	}
-}
+// func multiplyNumbers(c chan int, num int) {
+// 	c <- num * 10
+// 	// numbers := []int{1, 2, 3, 4, 5}
+// 	// for _, number := range numbers {
+// 	// 	fmt.Println(number)
+// 	// 	time.Sleep(1 * time.Second)
+// 	// }
+// 	// fmt.Println(num)
+// }
 
 func main() {
-	go printNumbers()
-	time.Sleep(6 * time.Second)
+	// c := make(chan int, 3)
+	// go multiplyNumbers(c, 5)
+	// go multiplyNumbers(c, 3)
+	// go multiplyNumbers(c, 1)
+
+	// for i := 0; i < 3; i++ {
+	// 	foo, ok := <-c
+	// 	if !ok {
+	// 		fmt.Println("an error occurred ")
+	// 	}
+	// 	fmt.Println(foo)
+	// }
+	j := make(chan int, 4)
+	go worker(j, 3)
+	go worker(j, 5)
+	go worker(j, 1)
+	go worker(j, 3)
+	for i := 0; i < 4; i++ {
+		result, ok := <-j
+		if !ok {
+			fmt.Println("An error occurred")
+		}
+		fmt.Println(result)
+	}
+
+}
+func worker(j chan int, results int) {
+
+	j <- results * 2
+
 }
