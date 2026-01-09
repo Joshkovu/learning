412f94afa12805b55c6b0d5ecdf99b30fb678a0a
Joshkovu <joashkuteesa223@gmail.com>
2026-01-09 10:57:12 +0300

    feat:Implemented an Http request

--- HTTP/main.go
+++ HTTP/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	// "http/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+// var (
+// 	videoService service.VideoService = service.New()
+// )
+
+func main() {
+	server := gin.Default()
+	server.GET("/posts", func(ctx *gin.Context) {
+
+	})
+	server.Run(":8080")
+}
